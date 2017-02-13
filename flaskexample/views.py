95c655afde639f34032df23a4dd23990d65be250
~feast~ <ntchemod@gmail.com>
2017-02-13 02:15:44 -0800

    Fixed transparency issue. Added emoji pic functionality

--- flaskexample/views.py
+++ flaskexample/views.py
@@ -20,9 +20,17 @@ import sys
 # import psycopg2
 global shortname
 global current_emoji
+
+# ubuntu paths
 app.config['RAW_IMAGE_FOLDER']='/home/ubuntu/application/tmp' #/Users/NVT/PycharmProjects/application2/flaskexample/static/images/RAW/'
 app.config['MODELS_FOLDER']='/home/ubuntu/application/flaskexample/models/'
 app.config['STATIC_FOLDER']='/home/ubuntu/application/flaskexample/static'
+
+# osX paths
+# app.config['RAW_IMAGE_FOLDER']='/Users/NVT/fomoji/tmp' #/Users/NVT/PycharmProjects/application2/flaskexample/static/images/RAW/'
+# app.config['MODELS_FOLDER']='/Users/NVT/fomoji/flaskexample/models/'
+# app.config['STATIC_FOLDER']='/Users/NVT/fomoji/flaskexample/static'
+
 app.config['ALLOWED_EXTENSIONS'] = set(['png', 'jpg', 'jpeg', 'gif','JPG'])
 app.config['EMOJIS']=["wink", "facepalm", "yell", "kiss", "smile"] # ORDER MATTERS!!!!!!
 
@@ -30,9 +38,16 @@ app.config['EMOJIS']=["wink", "facepalm", "yell", "kiss", "smile"] # ORDER MATTE
 #app.config['LDAmodel']=
 #vc = cv2.VideoCapture(0)
 fisherfaces = cv2.createFisherFaceRecognizer(0)
-facebox = cv2.CascadeClassifier("/home/ubuntu/application/flaskexample/models/haarcascade_frontalface_default.xml")
-# fisherfaces.load('/Users/NVT/PycharmProjects/INSIGHT_APP/fomoji/models/allfellows_detection_model.xml')
-fisherfaces.load('/home/ubuntu/application/flaskexample/models/fellows_and_00_detection_model.xml')
+
+#ubuntu paths
+facebox = cv2.CascadeClassifier("/Users/NVT/fomoji/flaskexample/models/haarcascade_frontalface_default.xml")
+#fisherfaces.load('/Users/NVT/PycharmProjects/INSIGHT_APP/fomoji/models/allfellows_detection_model.xml')
+fisherfaces.load('/Users/NVT/fomoji/flaskexample/models/fellows_and_00_detection_model.xml')
+
+# osX paths:
+# facebox = cv2.CascadeClassifier("/Users/NVT/fomoji/flaskexample/models/haarcascade_frontalface_default.xml")
+# # fisherfaces.load('/Users/NVT/PycharmProjects/INSIGHT_APP/fomoji/models/allfellows_detection_model.xml')
+# fisherfaces.load('/Users/NVT/fomoji/flaskexample/models/fellows_and_00_detection_model.xml')
 
 
 @app.route('/')
@@ -88,38 +103,51 @@ def populate():
     #return(str(select)) # just to see what select is
     img = Image.open(os.path.join(app.config['STATIC_FOLDER'],"images/SpeechBubble_empty.jpg"))
     draw = ImageDraw.Draw(img)
-    font = ImageFont.truetype(os.path.join(app.config['STATIC_FOLDER'],"fonts/Arial Unicode.ttf"), 74,
+    font = ImageFont.truetype(os.path.join(app.config['STATIC_FOLDER'],"fonts/Arial Unicode.ttf"), 72,
                                encoding='unic')
-    emofont=ImageFont.truetype(os.path.join(app.config['STATIC_FOLDER'],"fonts/NotoEmoji-Regular.ttf"), 74,
+    emofont=ImageFont.truetype(os.path.join(app.config['STATIC_FOLDER'],"fonts/NotoEmoji-Regular.ttf"), 72,
                                encoding='unic')
     margin = offset = 70
     if current_emoji=="smile":
-        emotext=emoji.emojize(':smile:')
+        emoji_image=Image.open(os.path.join(app.config['STATIC_FOLDER'],"images/smile_open.png"))
     elif current_emoji== "facepalm":
-        emotext=emoji.emojize(":confounded:")
+        emoji_image=Image.open(os.path.join(app.config['STATIC_FOLDER'],"images/facepalm_google.png"))
     elif current_emoji=="kiss":
-        emotext=emoji.emojize(":kissing_heart:")
+        emoji_image=Image.open(os.path.join(app.config['STATIC_FOLDER'],"images/kiss_open.png"))
     elif current_emoji=="yell":
-        emotext=emoji.emojize(":open_mouth:")
+        emoji_image=Image.open(os.path.join(app.config['STATIC_FOLDER'],"images/yell_open.png"))
     elif current_emoji=="wink":
-        emotext=emoji.emojize(":wink:")
+        emoji_image=Image.open(os.path.join(app.config['STATIC_FOLDER'],"images/wink_open.png"))
     else:
-        emotext=" "
-    mytext=mytext+emotext
+        emoji_image=[]
+
 
     if len(mytext)>1:
         for line in textwrap.wrap(mytext, width=30):
             draw.text((margin, offset), line, font=font, fill="#aa0000")
             offset += font.getsize(line)[1]
-    #    draw.text((margin, offset),emotext, font=emofont, fill="#aa0000")
-
     else:
         draw.text((margin, offset), '', font=font, fill="#aa0000")
+
+    emoji_image.thumbnail((100, 100), Image.ANTIALIAS)  # resizes image in-place
+    background = Image.new('RGBA', img.size, (255, 255, 255, 0))
+
+    xoff = 100
+    try:
+        img.paste(emoji_image, (xoff, offset), mask=emoji_image)
+    except ValueError:
+        pass
+
+    img = img.convert('RGBA')
+    background.paste(img, img)
+
     #here we want to save the bubble as the same name as the image it came from (but with word 'bubble' appended)
     bubble_name=shortname.rsplit('.', 1)[0] # split before .
     bubble_ext=shortname.rsplit('.', 1)[1] # split after .
     bubblewithtextname=bubble_name+"_bubble."+bubble_ext
-    img.save(os.path.join(app.config['RAW_IMAGE_FOLDER'], bubblewithtextname),"JPEG")
+
+    #now add emoji to corner!!!
+    background.save(os.path.join(app.config['RAW_IMAGE_FOLDER'], bubblewithtextname),"JPEG")
     print(bubblewithtextname)
     #return(current_emoji)
     # bubblewithtextname="sample-out.jpg"
@@ -151,7 +179,9 @@ def detect_face_box(small_frame,facebox):
             cv2.rectangle(small_frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
             #draw rectangle and save
             #NOTE: for some reason this thing detects boxes that are much wider than in training set. the 10 is to compensate...ugh
-            current_emoji,conf=classify_face(fisherfaces, small_grey_frame[y+10:y+h-10,x+10:x+w-10])
+            #current_emoji,conf=classify_face(fisherfaces, small_grey_frame[y+10:y+h-10,x+10:x+w-10])
+            current_emoji,conf=classify_face(fisherfaces, small_grey_frame[y:y+h,x:x+w])
+
             #current_emoji is a string...
             print(current_emoji,conf)
     return current_emoji,conf,small_frame
@@ -172,4 +202,9 @@ def classify_face(fisherfaces,small_grey_frame):
 @app.route('/tmp/<filename>')
 def uploaded_file(filename):
     return send_from_directory(app.config['RAW_IMAGE_FOLDER'],filename)
+
+@app.route('/project_info')
+def project_info():
+    return render_template('project_info.html')
+
 
